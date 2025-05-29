b8f0f7a31f2719a130d36a5690d1a0a61d21b164
Luis Enrique Sanchez Romero <luiskike2210@gmail.com>
2025-05-29 12:50:47 -0600

    fixing

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter as Route, Router, Routes } from "react-router-dom";
 import Navbar from "./components/header";
-import About from "./view/About";
-import Contact from "./view/Contact";
-import Home from "./view/Home";
 
 export default function App() {
   return (
-      <Router>
-      <Navbar />
-      <main className="p-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </main>
-    </Router>
+    //   <Router>
+    //   <Navbar />
+    //   <main className="p-4">
+    //     <Routes>
+    //       <Route path="/" element={<Home />} />
+    //       <Route path="/servicios" element={<Servicios />} />
+    //       <Route path="/contacto" element={<Contacto />} />
+    //     </Routes>
+    //   </main>
+    // </Router>
+    <></>
   )
 }
 
